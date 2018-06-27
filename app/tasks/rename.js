9487e16d01eaeb543f6d63a7e7c6247887415d9f
Soren Baird <sorenbaird@gmail.com>
2018-06-27 10:05:43 -0700

    Finished alpha of renaming functionality

--- app/tasks/rename.js
+++ app/tasks/rename.js
@@ -1,20 +1,39 @@
+const fs = require('fs');
+const beautify = require('js-beautify');
+
+const HTML_PATH = 'resources/html/pages';
+const SCSS_PATH = 'resources/scss/pages';
+const JS_PATH = 'resources/js/pages';
+const IMG_PATH = 'resources/img/pages';
+
 module.exports = (gulp, banners) => {
   gulp.task('rename', () => {
-    const currentNameIndex = process.argv.indexOf('--currentName') + 1;
+    const currentNameIndex = process.argv.indexOf('--old') + 1;
     const currentName = currentNameIndex ? process.argv[currentNameIndex] : null;
-    const newNameIndex = process.argv.indexOf('--newName') + 1;
+    const newNameIndex = process.argv.indexOf('--new') + 1;
     const newName = newNameIndex ? process.argv[newNameIndex] : null;
+
+    const checkThenRename = (currentPath, newPath) => {
+      if (fs.existsSync(currentPath)) {
+        fs.rename(currentPath, newPath);
+      }
+    }
     
     if (!currentName || !newName) {
-      console.log('PLEASE INPUT A VALID CURRENT NAME AND NEW NAME. THE COMMAND FORMAT SHOULD READ: "gulp rename --currentName <current-name> --newName <newName>"');
+      console.log('ERROR: Please input a valid existing name and a new name. THE COMMAND FORMAT SHOULD READ: "gulp rename --old <current-name> --new <newName>"');
       return;
+    } else if (!banners.banners[currentName]) {
+      console.log(`ERROR: There is no banner named ${currentName}. Please try selecting a valid banner.`);
+      return;
+    } else {
+      checkThenRename(`${HTML_PATH}/${currentName}.html`, `${HTML_PATH}/${newName}.html`);
+      checkThenRename(`${SCSS_PATH}/${currentName}.scss`, `${SCSS_PATH}/${newName}.scss`);
+      checkThenRename(`${JS_PATH}/${currentName}.js`, `${JS_PATH}/${newName}.js`);
+      checkThenRename(`${IMG_PATH}/${currentName}`, `${IMG_PATH}/${newName}`);
+      const bannerJson = JSON.stringify(banners).replace(currentName, newName);
+      fs.writeFileSync('banners.json', beautify(bannerJson, { indent_size: 2 }), 'utf8');
     }
-
-    if (!banners.banners[currentName]) {
-      console.log('NO BANNER WAS FOUND WITH THE CURRENT BANNER NAME');
-      return;
-    } 
     
-    console.log(currentName, newName);
+    console.log(`RENAME COMPLETE: ${currentName} was renamed to ${newName}`);
   });
 }
